50aa8bb9cefe1dccf40bd855c748751094aadbbf
UnaiH22 <53757754+UnaiH22@users.noreply.github.com>
2024-12-03 20:04:53 +0000

    garbiketa

--- public/javascripts/DASH_test.js
+++ public/javascripts/DASH_test.js
@@ -1,19 +1,17 @@
 
-(function () {
-    var url = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+(function(){
+    //url-a beidatu behar dan nola jarri ondo zerbitzarian dagoenean
+    var url = "../video/mpd/output.mpd";
     var videoElement = document.querySelector('.videoContainer video');
     var player = dashjs.MediaPlayer().create();
     player.initialize(videoElement, url, true);
-
     var controlbar = new ControlBar(player);
     controlbar.initialize();
 })();
-
 var videoContainer = document.getElementById('videoContainer');
-document.addEventListener('fullscreenchange', function () {
-    if (document.fullscreenElement) {
-        videoContainer.classList.add('fullscreen');
-    } else {
-        videoContainer.classList.remove('fullscreen');
-    }
-});
+document.addEventListener('fullscreenchange', function() { 
+    if (document.fullscreenElement) { 
+        videoContainer.classList.add('fullscreen'); 
+    } else { 
+        videoContainer.classList.remove('fullscreen'); 
+    } });
